0a62d260565a5f358f92c76c8c6d86ceb5f1b103
Mariam <kelanimaryam1234@gmail.com>
2024-10-18 08:45:09 +0100

    fixed the home on views folder

--- dashboard/src/views/Home.vue
+++ dashboard/src/views/Home.vue
@@ -2,14 +2,20 @@
   <main class="home-page">
     <h1>Home</h1>
     <p>This is the home page</p>
+    <!-- Add the MyChart component here -->
+    <MyChart />
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import MyChart from "@/components/MyChart.vue"; // Import the MyChart component
 
 export default defineComponent({
   name: "HomePage",
+  components: {
+    MyChart, // Register the MyChart component
+  },
 });
 </script>
 
